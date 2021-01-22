fe8b119c8b9fa95db0735bce649a9054365e05f8
mzxssg <707021560@qq.com>
2021-01-22 12:27:48 +0800

    more edit...

---
+++ src/main/java/com/atguigu/git/Happy.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Zexin Ma
+ * @Create: 2021-01-22-12:26
+ * @Description:
+ */
+public class Happy {
+    public static void main(String[] args) {
+        System.out.println("Test...");
+    }
+}
