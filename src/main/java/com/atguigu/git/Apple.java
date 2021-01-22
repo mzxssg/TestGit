6a8f44031e8b580a61bcfcfe59cf95edf3b78779
mzxssg <707021560@qq.com>
2021-01-22 11:56:05 +0800

    My fist commit

---
+++ src/main/java/com/atguigu/git/Apple.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+/**
+ * @Author: Zexin Ma
+ * @Create: 2021-01-22-10:38
+ * @Description:
+ */
+public class Apple {
+}
---
+++ src/test/java/com/atguigu/test/GoodTest.java
@@ -0,0 +1,9 @@
+package com.atguigu.test;
+
+/**
+ * @Author: Zexin Ma
+ * @Create: 2021-01-22-10:41
+ * @Description:
+ */
+public class GoodTest {
+}
